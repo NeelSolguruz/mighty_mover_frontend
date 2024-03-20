439042f2fadfe6ebc1f3430de772498159005c34
NeelSolguruz <neels@solguruz.com>
2024-03-20 20:07:29 +0530

    WIP:additional benefits

--- src/app/constant/constant.ts
+++ src/app/constant/constant.ts
@@ -1,104 +1,135 @@
 // import regularTrip from "../assets/Images/regularTrip.png"
+import {
+  regularTrip,
+  BetterEearning,
+  onTImePayment,
+} from "../assets/Images/imageassets";
 
 export const ENTERPRISE_STRING = {
-    WHY_USE_PORTER: 'Why use Porter for Enterprise?',
-    FOR_ENTERPRISE:"For Enterprise",
-    FORM_CITIES:[
-        "Ahmedabad",
-        "Banglore",
-        "Chandigarh",
-        "Chennai",
-        "Coimbatore",
-        "Delhi NCR",
-        "Hyderbad",
-        "Indore",
-        "Jaipur",
-        "Kochi",
-        "Kolkata",
-        "Lucknow",
-        "Ludhiana",
-        "Mumbai",
-        "Nagpur",
-        "Pune",
-        "Surat",
-        "Vadodara"
-
-    ]
-}
+  WHY_USE_PORTER: "Why use Porter for Enterprise?",
+  FOR_ENTERPRISE: "For Enterprise",
+  FORM_CITIES: [
+    "Ahmedabad",
+    "Banglore",
+    "Chandigarh",
+    "Chennai",
+    "Coimbatore",
+    "Delhi NCR",
+    "Hyderbad",
+    "Indore",
+    "Jaipur",
+    "Kochi",
+    "Kolkata",
+    "Lucknow",
+    "Ludhiana",
+    "Mumbai",
+    "Nagpur",
+    "Pune",
+    "Surat",
+    "Vadodara",
+  ],
+};
 
 export const DELIVERY_PARTNER_STRING = {
-    PARTNER_BANNER_LINE1: `Attach mini trucks or bike`,
-    PARTNER_BANNER_LINE2: `Earn money by delivering goods, courier and packages. Get a part time or full time delivery job`,
-    FORM_CITIES:[
-        "Ahmedabad",
-        "Banglore",
-        "Chandigarh",
-        "Chennai",
-        "Coimbatore",
-        "Delhi NCR",
-        "Hyderbad",
-        "Indore",
-        "Jaipur",
-        "Kochi",
-        "Kolkata",
-        "Lucknow",
-        "Ludhiana",
-        "Mumbai",
-        "Nagpur",
-        "Pune",
-        "Surat",
-        "Vadodara"
+  PARTNER_BANNER_LINE1: `Attach mini trucks or bike`,
+  PARTNER_BANNER_LINE2: `Earn money by delivering goods, courier and packages. Get a part time or full time delivery job`,
+  FORM_CITIES: [
+    "Ahmedabad",
+    "Banglore",
+    "Chandigarh",
+    "Chennai",
+    "Coimbatore",
+    "Delhi NCR",
+    "Hyderbad",
+    "Indore",
+    "Jaipur",
+    "Kochi",
+    "Kolkata",
+    "Lucknow",
+    "Ludhiana",
+    "Mumbai",
+    "Nagpur",
+    "Pune",
+    "Surat",
+    "Vadodara",
+  ],
+  FORM_VEHICLE: [
+    " TATA ACE ",
+    "TATA 407",
+    "PICKUP / DOST / SUPER ACE / 8FT",
+    "3 WHEELER",
+    "2 WHEELER",
+    "3 WHEELER ELECTRIC",
+    "TATA 407 14FT",
+    "OTHERSX",
+  ],
 
-    ],
-    FORM_VEHICLE:[
-        
-       " TATA ACE ",
-        "TATA 407",
-        "PICKUP / DOST / SUPER ACE / 8FT",
-        "3 WHEELER",
-        "2 WHEELER",
-        "3 WHEELER ELECTRIC",
-        "TATA 407 14FT",
-        "OTHERSX",
-    ],
+  FORM_SOURCES: [
+    "BUS ADS",
+    "PAMPHLET",
+    " GARAGE HOARDING",
+    "NEWSPAPER ADS",
+    "DRIVER REFERRAL",
+    "CUSTOMER REFERRAL",
+    "CHANNEL PARTNER",
+    "OTHERS",
+    "FOS",
+    "ONLINE/FACEBOOK",
+    "SMART SHIFT",
+  ],
 
-    FORM_SOURCES: [
-        "BUS ADS",
-        "PAMPHLET",
-        " GARAGE HOARDING",
-        "NEWSPAPER ADS",
-        "DRIVER REFERRAL",
-        "CUSTOMER REFERRAL",
-        "CHANNEL PARTNER",
-        "OTHERS",
-        "FOS",
-        "ONLINE/FACEBOOK",
-        "SMART SHIFT",
-    ],
+  FORM_REGISTRATION_BUTTONS: "register",
 
-    FORM_REGISTRATION_BUTTONS: "register",
+  FORM_HEADERS: `Attach Vehicle Now`,
 
-    FORM_HEADERS:`Attach Vehicle Now`,
+  PORTER_ADVANTAGE: "PORTER ADVANTAGE",
 
-    PORTER_ADVANTAGE:"PORTER ADVANTAGE",
-
-    PORTER_ADVANTAGE_DATA: [
-        {
-        // images:{regularTrip: "../assets/Images/regularTrip.png"},
-        title: "Regular Trips",
-        description: "With our growing presence across multiple cities, we always have our hands full! This means you will never run out of trips.",
-        
+  PORTER_ADVANTAGE_DATA: [
+    {
+      images: regularTrip,
+      title: "Regular Trips",
+      description:
+        "With our growing presence across multiple cities, we always have our hands full! This means you will never run out of trips.",
     },
     {
-        title: "Better Earning",
-        description: "Earn more by partnering with the best! Regular trips and efficient service can grow your earnings!",
-        icon: "fas fa-check"
+      title: "Better Earning",
+      description:
+        "Earn more by partnering with the best! Regular trips and efficient service can grow your earnings!",
+      images: BetterEearning,
     },
     {
-        title: "On-Time Payment",
-        description: "Be assured to receive all payments on time & get the best in class support, when you attach mini truck with Porter.",
-        icon: "fas fa-check"
-    }
-]
+      title: "On-Time Payment",
+      description:
+        "Be assured to receive all payments on time & get the best in class support, when you attach mini truck with Porter.",
+      images: onTImePayment,
+    },
+  ],
+  MAKING_YOUR_LIFE_EASY:"MAKING YOUR LIFE EASY",
+  MAKING_YOUR_LIFE_EASY_DATA: `Attach your two wheeler or commercial vehicle\nIf you got a 2 wheeler, or a tata ace commercial vehicle, you are good to go! With Porter, get a delivery job and deliver goods, packages, and courier.\n\n\n### No more waiting on the stand\nAttach tata ace with Porter to have a steady stream of trips with minimum assured income and added incentives, so that there is no waiting and idle time at the stand!\n\n### No more bargaining. Standard Rates\nThe rates and calculation methods are standardized and completely transparent. No more wasting time in fixing the rates for every trip.\n\n\n### Hassle Free Navigation\nWith our GPS-based navigation you can drive anywhere across your city without worrying about the directions. Get real-time navigation assistance on the go!`,
 
-}
+  ADDITIONAL_BENEFITS: [
+    {
+        images: regularTrip,
+      title: "Health Care Assistance",
+      description:
+        "Get healthcare benefits for you and your family with mini truck attachment.",
+    },
+    {
+        title: "Insurance",
+        description:"Save money with reduced annual maintenance and insurance costs on your vehicle.",
+        images: BetterEearning,
+    },
+    {
+        title: "Fuel card for Savings",
+        description:"Save big on fuel costs with our smart fuel card and increase your profit margins!",
+        images: onTImePayment,
+    },
+    {
+        images: regularTrip,
+      title: "Discount on Vehicle Purchase",
+      description:"Get major discounts on purchase of new vehicles. Add to your fleet and grow your business!"
+    },
+  ]
+
+
+};
--- src/app/deliveryPartner/page.tsx
+++ src/app/deliveryPartner/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
 import Image from "next/image";
 import { useState } from "react";
 import { DELIVERY_PARTNER_STRING } from "../constant/constant";
-import Delivery from "../assets/Images/delivery.jpg";
+// import Delivery from "../assets/Images/delivery.jpg";
 import { FormData } from "../dto/data.type";
-import  regularTrip from "../assets/Images/regularTrip.png"
+// import  regularTrip from "../assets/Images/regularTrip.png"
+import { Delivery, driverExperience } from "../assets/Images/imageassets";
 
 export default function DeliveryPage(): JSX.Element {
   const [formData, setFormData] = useState<FormData>({
@@ -52,7 +52,7 @@ export default function DeliveryPage(): JSX.Element {
         </div>
       </div>
       <div className="w-full">
-        <div className="text-3xl font-semibold text-center w-full">
+        <div className="text-3xl font-semibold text-center w-full p-8">
           {DELIVERY_PARTNER_STRING.PORTER_ADVANTAGE}
         </div>
       </div>
@@ -61,25 +61,34 @@ export default function DeliveryPage(): JSX.Element {
           <div className="w-1/2 flex gap-10 ml-24">
             {DELIVERY_PARTNER_STRING.PORTER_ADVANTAGE_DATA.map((item) => (
               <>
-                <div className="flex flex-col justify-center gap-8 items-center p-2 w-2/5 ">
-                  <div>
+                <div
+                  key={item.title}
+                  className="flex flex-wrap flex-col justify-center gap-8 items-center p-2 w-full  md:w-2/5 lg:w-1/3"
+                >
+                  <div className="divForImage w-4/5 h-1/2 ">
+                   <div className=" w-full h-full ">
                     <Image
-                      src={regularTrip}
-                      alt="Logo"
-                      className="w-42 h-42 rounded-full shadow-gray-400 shadow-md transition-all hover:scale-105"
+                      src={item.images}
+                      alt={item.title}
+                      className="w-full h-full rounded-lg  object-contain shadow-gray-400 shadow-md transition-all hover:scale-105"
+                      
                     ></Image>
+                   </div>
                   </div>
-                  <div className="text-xl font-semibold w-auto">
-                    {item.title}
-                  </div>
-                  <div className="flex text-sm font-light text-center">
-                    {item.description}
+                  <div className="divForDescription w-18 p-2">
+                    <div className="divForTitle text-center text-xl font-semibold w-auto">
+                      {item.title}
+                    </div>
+                    <div className="divForDes flex  text-sm font-light text-center">
+                      {item.description}
+                    </div>
                   </div>
                 </div>
               </>
             ))}
           </div>
-          <div className="mr-14 w-4/12 p-10 rounded-3xl shadow-gray-400 shadow-md">
+
+          <div className="mr-14 w-full  md:w-4/12 p-10 rounded-3xl shadow-gray-400 shadow-md">
             <form onSubmit={handleSubmit}>
               <div className="flex flex-col gap-4 p-6">
                 <div className="font-bold text-2xl text-center w-full">
@@ -104,15 +113,23 @@ export default function DeliveryPage(): JSX.Element {
                       Name
                     </label>
                   </div>
-                  <input
-                    type="number"
-                    name="mobileNumber"
-                    value={formData.mobileNumber}
-                    onChange={handleChange}
-                    placeholder="Mobile Number"
-                    className="w-full border-b rounded border-r-2 border-gray-300 py-1 focus:border-b-2 focus:border-amber-800 transition-colors focus:outline-none peer"
-                    required
-                  />
+                  <div className="relative">
+                    <input
+                      type="number"
+                      id="mobilenumber"
+                      name="mobileNumber"
+                      value={formData.mobileNumber}
+                      onChange={handleChange}
+                      className="w-full border-b rounded border-r-2 border-gray-300 py-1 focus:border-b-2 focus:border-amber-800 transition-colors focus:outline-none peer"
+                      required
+                    />
+                    <label
+                      htmlFor="mobilenumber"
+                      className="absolute pl-2 left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all font-semibold"
+                    >
+                      Mobile Number
+                    </label>
+                  </div>
                   <select
                     name="city"
                     value={formData.city}
@@ -166,6 +183,44 @@ export default function DeliveryPage(): JSX.Element {
           </div>
         </div>
       </div>
+      <div className="w-full bg-black text-white h-auto">
+        <div className="text-3xl font-semibold text-center w-full p-8 text-white">
+            {DELIVERY_PARTNER_STRING.MAKING_YOUR_LIFE_EASY}
+          </div>
+        <div className="mainSection bg-black w-full">
+          <div className="float-left w-full h-96 bg-black  flex gap-10 items-center justify-center">
+            <div className=" w-1/3 ">
+              <Image
+              src={driverExperience}
+              // className="w-auto bg-black"
+              className="w-full h-full rounded-lg  object-contain  shadow-md transition-all hover:scale-105"
+              alt="Picture of the Delivery"
+              ></Image>
+            </div>
+            <div className="w-2/5 font-semibold text-wrap  flex flex-wrap">
+              {DELIVERY_PARTNER_STRING.MAKING_YOUR_LIFE_EASY_DATA}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ADDITIONAL BENEFITS __ start from here */}
+      <div className="w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 p-10">
+        {DELIVERY_PARTNER_STRING.ADDITIONAL_BENEFITS.map((item) => (
+          <div key={item.title} className="flex flex-col justify-center gap-8 items-center">
+            <div className="divForImage w-full h-48 relative">
+              <Image src={item.images} alt={item.title} layout="fill" objectFit="cover" className="rounded-lg shadow-gray-400 shadow-md transition-all hover:scale-105" />
+            </div>
+            <div className="divForDescription p-2">
+              <div className="divForTitle text-center text-xl font-semibold">{item.title}</div>
+              <div className="divForDes text-sm font-light text-center">{item.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+
     </>
   );
 }
