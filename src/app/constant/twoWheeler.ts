277d0396d9d81124f29cff9af93fa2a88ce61c1f
NeelSolguruz <neels@solguruz.com>
2024-03-29 16:17:59 +0530

    two-wheeler

---
+++ src/app/constant/twoWheeler.ts
@@ -0,0 +1,124 @@
+import {
+  small_truck1,
+  small_truck2,
+  small_truck3,
+  big_truck1,
+  big_truck2,
+  big_truck3,
+  banglore,
+  chandigarh,
+  coim,
+  delhi_trucks,
+  indore,
+  hyderbad,
+  jaipur,
+  kanpur,
+  kochi,
+  lucknow,
+  ludhiana,
+  mumbai,
+  nagpur,
+  chennai_truck,
+  surat,
+  vadodara,
+  pune,
+  kolkata,
+  ahmedabad_truck,
+  bike_image,
+  Bike,
+} from "../assets/Images/imageassets";
+
+export const BIKE_MAIN_PAGE_DATA = [
+  {
+    id: "Ahmedabad",
+    title: "Pickup and Drop Packages Services in Ahmedabad",
+    desc: "Book Porter bikes for all your package delivery services. Download the app for reliable and safe deliveries of your packages in the city.",
+  },
+];
+export const BIKE_KNOW_MORE = "Know More >";
+export const RENT_MINI_TRUCKS_IN_AHMEDABAD = "Two-Wheelers from Mighty Movers";
+export const BIKE_DATA = [
+  {
+    img: Bike,
+    name: "2 Wheeler",
+    weight: "20kg",
+    prefix: "Starting from ₹",
+    desc: "Base fare is inclusive of 1.0 km distance & 25 minutes of order time. Pricing may vary basis locality. Please note, road tax, parking fee, etc, will be applicable over and above ride fare.",
+    price: "32.5",
+  },
+];
+
+export const BIKE_DESC_STRING = "Why Choose Porter Pickup & Delivery Services?";
+export const BIKE_DESC_STRING_VALUE = [
+  {
+    id: 1,
+    title: "Door-to-door Services",
+    desc: "Enjoy on-demand pickup & delivery services at your doorstep",
+  },
+  {
+    id: 2,
+    title: "Get Instant Deliveries",
+    desc: "Get your documents, laptops, lunch boxes, forgotten keys or other small packages picked up and delivered instantly",
+  },
+  {
+    id: 3,
+    title: "Get Anything Delivered",
+    desc: "Be it a pen or multiple documents, send anything up to 20 kg anywhere across the city",
+  },
+  {
+    id: 4,
+    title: "Reliable Same-day Delivery",
+    desc: "Book two-wheeler delivery services online and get same-day delivery",
+  },
+];
+
+export const AREAS_WE_SERVE_IN = "Areas We Serve";
+export const AREAS_WE_SERVE_IN_DATA = [
+  "Aslali",
+
+  "Bapunagar",
+
+  "Bopal",
+
+  "CTM",
+
+  "Maninaga",
+
+  "Naroda",
+
+  "Naroda GIDC",
+
+  "Odhav",
+
+  "Sarkhej",
+
+  "Thaltej",
+
+  "Usman Pura",
+
+  "Vasna Or Vatva",
+];
+export const THINK_lOGISTICS_THINK_PORTER =
+  "Think Logistics, Think Like Mighty Mover!";
+export const GET_ORDERS =
+  "Get the Mighty Movers mobile app to start booking your orders!";
+export const SCAN_TO_DOWNLOAD = "Scan to download our app!";
+export const CHOOSE_FROM_OUR_SERVICES = "Choose from our services";
+export const POPULAR_CATEGORIES = "Popular Categories";
+export const POPULAR_CATEGORIES_DATA = [
+  "Rent 3 Wheeler In Ahmedabad",
+
+  "Rent Pickup 8FT In Ahmedabad",
+
+  "Rent Tata Ace In Ahmedabad",
+
+  "Rent Canter 14FT In Ahmedabad",
+
+  "Truck Rental",
+
+  "Rent 1.7 Ton Pick Up In Ahmedabad",
+];
+export const EXPLORE_TRUCK_RENTAL = "Explore Truck Rental Services in India";
+export const RENT_MINI_TRUCK = "Rent Mini Trucks In";
+export const IN_CASE_YOU_HAVE_MORE_QUESTION =
+  "In case you have more questions, feel free to reach out to us.";
