3762e9b18a3917325fd7ccb87a9daff448baca7c
NeelSolguruz <neels@solguruz.com>
2024-04-01 11:04:10 +0530

    twowheller responsive on working

--- src/app/two-wheelers/page.tsx
+++ src/app/two-wheelers/page.tsx
@@ -21,6 +21,7 @@ import {
   ENTERPRISE_STRING,
   DELIVERY_PARTNER_STRING,
   IN_CASE_YOU_HAVE_MORE_QUESTION,
+  CITIES_TRUCK,
   // KNOW_MORE,
 } from "../constant/constant";
 import {
@@ -56,9 +57,9 @@ function twoWheeler() {
 
   return (
     <>
-      <div className="w-full h-auto bg-gray-100 container md:container">
+      <div className="w-full bg-gray-100">
         <div className="w-full relative ">
-          <div className="md:w-full h-96">
+          <div className="md:w-full md:h-96 h-96">
             <Image
               src={truck_page_indi}
               alt="Bike main page"
@@ -66,11 +67,11 @@ function twoWheeler() {
               // width={250}
               // height={250}
               // objectFit="cover"
-              className="h-96"
+              className="w-full h-full"
             />
           </div>
-          <div className="flex flex-col gap-4 w-full justify-center text-center absolute top-8 max-tablet:top-2 max-middle:top-14 max-s:top-10">
-            <div className="text-white font-semibold text-4xl max-tablet:text-3xl max-s:text-2xl">
+          <div className="flex inset-0  flex-col gap-4 w-full justify-center text-center absolute top-8 max-tablet:top-2 max-middle:top-14 max-s:top-10">
+            <div className="text-white font-semibold text-4xl md:text-3xl sm:text-2xl">
               {BIKE_MAIN_PAGE_DATA[0].title}
             </div>
             <div className="w-full text-white font-medium text-xl flex justify-center max-tablet:text-lg max-middle:text-md">
@@ -91,25 +92,18 @@ function twoWheeler() {
                 className="w-48 bg-orange-100 border-2 border-orange-500 flex justify-center items-center p-2 rounded-lg transition-all hover:scale-90"
                 onClick={showModal}
               >
-                <div className="text-l font-bold text-orange-500">
+                <button className="text-l font-bold text-orange-500">
                   Please Select Your city
-                </div>
+                </button>
               </div>
               {isModalOpen && (
-                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-                  <div className="bg-white p-4 rounded-lg">
-                    <div
-                      style={{
-                        textAlign: "center",
-                        fontSize: "1.5rem",
-                        fontWeight: "bold",
-                        color: "#333",
-                      }}
-                    >
+                <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
+                  <div className="bg-white p-4 rounded-lg max-w-md">
+                    <div className="font-bold text-xl mb-4">
                       Choose Your City
                     </div>
                     <div className="flex flex-wrap gap-4 justify-center items-center">
-                      {CITIES.map((item: any) => (
+                      {CITIES_TRUCK.map((item: any) => (
                         <Link href={`/trucks/${item.name}`} key={item.name}>
                           <div className="flex flex-col gap-2" key={item.name}>
                             <div className="w-20 h-20 relative">
@@ -153,80 +147,43 @@ function twoWheeler() {
               {RENT_BIKE_IN_AHMEDABAD}
             </div>
           </div>
-          {/* <div className="justify-center items-center gap-4">
-            {BIKE_DATA.map((item) => (
-              <>
-                <div className="bg-white h-auto w-auto p-8 flex flex-col gap-2  justify-center items-center border-[1px] border-gray-400">
-                  <div>
-                    <Image
-                      src={item.img}
-                      alt="mini_truck"
-                      className="w-full"
-                    ></Image>
-                  </div>
-                  <div className="w-auto bg-[#EEF2FF] text-black flex p-1 rounded-lg">
-                    <FaWeightHanging className="text-sm mt-1 mr-1.5" />{" "}
-                    {item.weight}
-                  </div>
-                  <div className="font-semibold text-black text-xl">
-                    {item.name}
-                  </div>
-                  <div>
-                    {item.prefix}{" "}
-                    <span className="text-black text-md font-bold">
-                      {item.price}
-                    </span>
-                  </div>
-                  <div className="font-semibold text-black text-xl">
-                    {item.desc}
-                  </div>
-                  <div></div>
-                </div>
-              </>
-            ))}
-          </div> */}
-          <div className="w-full flex justify-center ">
-            <div className="w-1/2 bg-white flex justify-between items-center border-2 ">
+
+          <div className="w-full flex justify-center">
+            <div className="w-full md:w-1/2 bg-white flex justify-between items-center border-2">
               {BIKE_DATA.map((item) => (
-                <>
-                  <div className="bg-white w-full flex flex-row gap-5 justify-center items-center border-[1px] border-gray-400">
-                    <div className="w-1/2 px-8">
-                      <Image
-                        src={item.img}
-                        alt="mini_truck"
-                        className="w-9/12 "
-                      ></Image>
+                <div
+                  key={item.name}
+                  className="bg-white w-full flex flex-row gap-5 justify-center items-center border-[1px] border-gray-400"
+                >
+                  <div className="w-1/2 px-8">
+                    <Image src={item.img} alt="mini_truck" className="w-9/12" />
+                  </div>
+                  <div className="w-1/2 flex flex-col py-8">
+                    <div className="font-semibold text-black text-xl">
+                      {item.name}
                     </div>
-                    <div className="w-1/2 flex flex-col py-8">
-                      <div className="font-semibold text-black text-xl">
-                        {item.name}
-                      </div>
-                      <div className="bg-[#EEF2FF] text-black flex p-1 rounded-lg w-24">
-                        <FaWeightHanging className="text-sm mt-1 mr-1.5" />{" "}
-                        {item.weight}
-                      </div>
-                      <div className="font-semibold text-md ">
-                        {item.prefix}{" "}
-                        <span className="text-black text-md font-bold">
-                          {item.price}
-                        </span>
-                      </div>
-                      <div className="font-normal text-black text-md">
-                        {item.desc}
-                      </div>
-                      <div></div>
+                    <div className="bg-[#EEF2FF] text-black flex p-1 rounded-lg w-24">
+                      <FaWeightHanging className="text-sm mt-1 mr-1.5" />{" "}
+                      {item.weight}
+                    </div>
+                    <div className="font-semibold text-md">
+                      {item.prefix}{" "}
+                      <span className="text-black text-md font-bold">
+                        {item.price}
+                      </span>
+                    </div>
+                    <div className="font-normal text-black text-md">
+                      {item.desc}
                     </div>
                   </div>
-                </>
+                </div>
               ))}
             </div>
           </div>
         </div>
 
         {/* bike services */}
-        <div className="w-full mb-4 mt-2">
-          
-        </div>
+        <div className="w-full mb-4 mt-2"></div>
       </div>
     </>
   );
