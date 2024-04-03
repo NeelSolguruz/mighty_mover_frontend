cde4bf6142b4e69de5182801cd95fe8886e50f01
utkarsh <utkarshv@solguruz.com>
2024-04-03 18:44:19 +0530

    folder structure changed for certain pages where navbar and footer is not required

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+import "@/app/globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { Titillium_Web } from "next/font/google";
+
+const TitilliumWeb = Titillium_Web({
+    weight: ["400", "600", "700", "900", "200", "300"],
+    subsets: ["latin"],
+    display: "swap",
+});
+// const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Mighty Movers",
+    description: "Created by 3 fresh developers",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={TitilliumWeb.className}>
+                <Navbar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
+}
