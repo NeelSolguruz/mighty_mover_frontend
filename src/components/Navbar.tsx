34dba0590c3cbf71893b819cb72a79aa6b28a3c0
utkarsh <utkarshv@solguruz.com>
2024-04-04 19:01:09 +0530

    changes in navbar and footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { FaUserCircle } from "react-icons/fa";
 import { IoMenu } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
 import { useState } from "react";
-import { BrandLogo, porter_logo } from "../assets/Images/imageassets";
+import NavLogo from "@/assets/Images/icons/NavLogo";
 import { NAVBAR } from "@/constant/constant";
 
 export default function Navbar() {
@@ -21,26 +21,33 @@ export default function Navbar() {
   return (
     <>
       <div className={`sticky top-0 flex shadow-md  border-2 w-full  h-16 items-center  justify-between bg-white text-zinc-900 z-10 `}>
-        <div className="p-2">
+        <div className="ml-10 max-md:ml-2">
           <Link href="/">
-            <BrandLogo />
+            <div className="w-[130px]">
+              <NavLogo />
+            </div>
           </Link>
         </div>
 
-        <div className="text-base flex gap-10 max-md:hidden">
+        <div className="flex gap-10 text-base max-md:hidden">
           {NAVBAR.LAPTOP.map((item, index) => (
             <Link key={index} href={item.url} className={item.class}>
               {item.text}
             </Link>
           ))}
-
         </div>
-        <div className="max-md:hidden" title="Log in/ Sign up">
+
+
+
+        <div className="flex justify-center max-md:hidden w-1/12">
           <Link href="/login">
             <FaUserCircle
-              className="w-20 size-10 hover:text-blue-500 hover:scale-105 transition-all"
+              className="size-10 hover:text-[#2967ff] hover:scale-125 transition-all"
             />
           </Link>
+          {/* <div>
+            <Link href='/register'><button className="bg-[#2967ff] p-3 rounded-lg text-white font-semibold transition duration-300 hover:bg-blue-500 hover:scale-105">Get Started</button></Link>
+          </div> */}
         </div>
 
         {/* side panel */}
