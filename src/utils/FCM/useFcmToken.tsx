cd2e46b6d2c415b2f0c552166133c140ce3ed161
utkarsh <utkarshv@solguruz.com>
2024-04-22 18:50:51 +0530

    new files commit

--- src/utils/FCM/useFcmToken.tsx
+++ src/utils/FCM/useFcmToken.tsx
@@ -1,8 +1,36 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import { getMessaging } from "firebase/messaging";
+import firebaseApp from "../firebase/firebase";
+import { getToken } from "firebase/messaging";
 
 
 const useFcmToken = () => {
-    useEffect(()=>{
-        
-    },[])
-}
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        const retrieveToken = async () => {
+            try {
+                const messaging = getMessaging(firebaseApp);
+                const currentToken = await getToken(messaging, { vapidKey: "BBP1UJVh9UxGFe7gLtNQaTddc-DwPVb7jLWes9_mKMKcujxLiHEJ-yFocWwyM_fZ770UjlhVuw8wuEpvvbHNyuk" });
+                if (currentToken) {
+                    setToken(currentToken);
+                } else {
+                    console.error('No FCM token available');
+                }
+            } catch (error) {
+                console.error('Error retrieving FCM token:', error);
+            }
+        };
+
+
+        retrieveToken();
+    }, []);
+
+    return token;
+};
+
+export default useFcmToken;
+
+
+
+
