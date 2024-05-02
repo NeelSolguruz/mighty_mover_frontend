19d70fa5fbea74ac857a4e4fbaae18f5a05d4069
NeelSolguruz <neels@solguruz.com>
2024-05-02 15:55:02 +0530

    delivery-driver

--- src/http/staticTokenService.ts
+++ src/http/staticTokenService.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
-import { error } from "console";
+// import { error } from "console";
 const config = {
-  baseURL: "http://192.168.68.68:3000",
+  baseURL: "http://192.168.68.78:3000",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
